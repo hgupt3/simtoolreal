c2ed203ad89fa57d20d8f34cf33ffcb20ec2f777
Tyler Lum <tylergwlum@gmail.com>
2026-05-24 18:38:11 -0700

    Simtoolreal adjust -5mm and 5mm in x and y

--- deployment/goal_pose_node.py
+++ deployment/goal_pose_node.py
@@ -349,7 +349,8 @@ def main():
             # [x+0.005, y+0.005, z, qx, qy, qz, qw]
             # [x+0.0075, y+0.01, z-0.015, qx, qy, qz, qw]
             # [x+0.0075, y+0.01, z, qx, qy, qz, qw]
-            [x, y, z, qx, qy, qz, qw]
+            # [x, y, z, qx, qy, qz, qw]
+            [x-0.005, y+0.005, z, qx, qy, qz, qw]
             for x, y, z, qx, qy, qz, qw in goal_poses_robot_frame
         ]
         # HACK: Start higher and interpolate down
